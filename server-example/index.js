c232a8ff2fd31d18ea768c49a06075d58375bdab
Himindukularathne <himindu.21@cse.mrt.ac.lk>
2024-04-16 06:20:57 +0530

    example-server for having good understand

---
+++ server-example/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+//methods
+
+app.get('/users/:userId/books/:bookId', (req, res) => {
+    res.send(req.params)
+
+})
+
+
+
+
+
+
+
+
+
+// requirements of todo api
+
